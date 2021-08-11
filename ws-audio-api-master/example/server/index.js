35476c3649ca89bfe78391a770d4c047d74d6341
Chardon François <francoischardon.contact@gmail.com>
2021-08-11 20:17:46 +0200

    var=>const/let

--- ws-audio-api-master/example/server/index.js
+++ ws-audio-api-master/example/server/index.js
@@ -1,61 +1,31 @@
-var https = require('http')
-var fs = require('fs')
-var WebSocketServer = require('ws').Server
+const https = require('http')
+const fs = require('fs')
+const WebSocketServer = require('ws').Server
 
-var wsPort = 8080
-var masterId
-var listeners = {}
+const wsPort = 8080
 
-var httpsServer = https
+const httpsServer = https
   .createServer({
     key: fs.readFileSync('key.pem', 'utf8'),
     cert: fs.readFileSync('cert.pem', 'utf8'),
   })
   .listen(wsPort)
 
-var wss = new WebSocketServer({ server: httpsServer })
+const wss = new WebSocketServer({ server: httpsServer })
 
-wss.on('connection', function (ws, req) {
-  var connectionId = req.headers['sec-websocket-key']
-  var isMaster = false
+wss.on('connection', (ws, req) => {
+  let connectionId = req.headers['sec-websocket-key']
 
-  if (!masterId) {
-    masterId = connectionId
-    isMaster = true
-    ws.on('message', function (message) {
-      console.log(message)
-      // send data to --> Vosk API //Google Speech API // CommonVoice // ...
-      // --> gives text back (transcription)
-      for (var cid in listeners) {
-        listeners[cid].send(
-          message,
-          {
-            binary: true,
-          },
-          function (err) {
-            if (err) {
-              console.log('Error: ', err)
-            }
-          }
-        )
-      }
-    })
-    console.log('Speaker connected')
-  } else {
-    listeners[connectionId] = ws
-    isMaster = false
-    console.log('Listener connected')
-  }
-
-  ws.on('close', function () {
-    if (isMaster) {
-      masterId = null
-      console.log('Speaker disconnected')
-    } else {
-      delete listeners[connectionId]
-      console.log('Listener disconnected')
-    }
+  wss.on('message', message => {
+    console.log(message)
+    // send data to --> Vosk API //Google Speech API // CommonVoice // ...
+    // --> gives text back (transcription)
   })
+  console.log('Speaker connected')
+})
+
+wss.on('close', () => {
+  console.log('Speaker disconnected')
 })
 
 console.log('Listening on port:', wsPort)
